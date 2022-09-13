9604a16a7047c5cbde0edce8403dc81808731edd
elias <eliasmaximus@gmail.com>
2022-09-13 18:42:46 -0300

    final version

--- week1/personalassignment1/server.js
+++ week1/personalassignment1/server.js
@@ -1,10 +1,13 @@
-const http = require('http')
+const express = require('express')
 const port = 3000
+const server = express()
 
-const server = http.createServer(function(req,resp){
-    resp.write('Elias Maximo')
-    resp.end()
-})
+//const server = http.createServer(function(req,resp){
+//    resp.write('Elias Maximo')
+//    resp.end()
+//})
+
+server.use('/',require('./routes'))
 
 server.listen(port, function(error){
     if(error){
