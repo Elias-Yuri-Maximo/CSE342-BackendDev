bc0b0093e5421de592886e9ee48880ef2d423e8c
elias <eliasmaximus@gmail.com>
2022-09-21 23:52:19 -0300

    Working

---
+++ week2/personalAssingment35/controllers/contacts.js
@@ -0,0 +1,87 @@
+const mongodb = require('../db/connection');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res, next) => {
+  console.log('all')
+  const result = await mongodb.getDb().db('personalAssignment').collection('contacts').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
+
+const getSingle = async (req, res, next) => {
+  console.log('Single')
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db('personalAssignment')
+    .collection('contacts')
+    .find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+};
+
+const createContact = async (req, res) => {
+  console.log('adding new contact')
+  console.log('Body '+req.body)
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+ 
+  const response = await mongodb.getDb().db('personalAssignment').collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db('personalAssignment')
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  console.log(userId)
+  const response = await mongodb.getDb().db('personalAssignment').collection('contacts').deleteOne({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
+};
---
+++ week2/personalAssingment35/routes/contacts.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const contactsController = require('../controllers/contacts')
+
+router.get('/',contactsController.getAll);
+
+router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
+module.exports = router;
